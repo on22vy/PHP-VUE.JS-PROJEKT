3f5adce78f56ed816f4f1c168f154501baddd75c
Acelya <acelya.cagan@gmail.com>
2023-10-24 20:40:00 +0200

    Change method for downloadFile

--- frontend/src/components/ListStructure.vue
+++ frontend/src/components/ListStructure.vue
@@ -11,19 +11,17 @@ export default {
   },
   methods: {
  
-    downloadFile() {
-      // Erstelle einen Link zum Herunterladen der Datei
-      const fileUrl = 'URL_DER_DATEI'; // Passe dies an die URL deiner Datei an
+    downloadFile(file) {
       const a = document.createElement('a');
-      a.href = fileUrl;
-      a.download = 'dateiname.extension'; // Passe dies an den Dateinamen und die Erweiterung an
-      document.body.appendChild(a);
+  a.href = file.url;
+  a.download = file.name; // Verwende den Dateinamen der aktuellen Datei
+  document.body.appendChild(a);
 
-      // Klicke auf den Link, um den Download zu starten
-      a.click();
+  // Klicke auf den Link, um den Download zu starten
+  a.click();
 
-      // Entferne den Link aus dem Dokument, nachdem der Download abgeschlossen ist
-      document.body.removeChild(a);
+  // Entferne den Link aus dem Dokument, nachdem der Download abgeschlossen ist
+  document.body.removeChild(a);
     },
     deleteFile(index) {
       // mit dieser Methode lösche ich das Element aus dem Array
@@ -118,7 +116,7 @@ export default {
             <path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/>
           </svg>
           </button>
-          <button @click="downloadFile" class="download-button">
+          <button @click="() => downloadFile(file)" class="download-button">
            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
           <path d="M288 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V274.7l-73.4-73.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l128 128c12.5 12.5 32.8 12.5 45.3 0l128-128c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L288 274.7V32zM64 352c-35.3 0-64 28.7-64 64v32c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V416c0-35.3-28.7-64-64-64H346.5l-45.3 45.3c-25 25-65.5 25-90.5 0L165.5 352H64zm368 56a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/>
            </svg>
