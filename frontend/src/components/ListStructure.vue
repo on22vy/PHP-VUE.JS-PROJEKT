2d1b0f587923cae7f52c1cdeb43a2462f0992482
Acelya <acelya.cagan@gmail.com>
2023-10-24 18:37:28 +0200

    Add delete icon instead of delete button

--- frontend/src/components/ListStructure.vue
+++ frontend/src/components/ListStructure.vue
@@ -39,6 +39,7 @@ export default {
 </script>
 
 <template>
+  
   <div>
     <!-- Navigationsleiste links-->
     <nav class="sidebar">
@@ -74,8 +75,12 @@ export default {
           <div class="file-name">{{ file.name }}</div>
           <div class="last-changed-date">{{ file.lastChangedDate }}</div>
           <div class="user-name">{{ file.userName }}</div>
-          <button @click="deleteFile(index)" class="delete-button">Löschen</button>
-          
+          <button @click="deleteFile(index)" class="delete-button">
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+            <path d="M170.5 51.6L151.5 80h145l-19-28.4c-1.5-2.2-4-3.6-6.7-3.6H177.1c-2.7 0-5.2 1.3-6.7 3.6zm147-26.6L354.2 80H368h48 8c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V432c0 44.2-35.8 80-80 80H112c-44.2 0-80-35.8-80-80V128H24c-13.3 0-24-10.7-24-24S10.7 80 24 80h8H80 93.8l36.7-55.1C140.9 9.4 158.4 0 177.1 0h93.7c18.7 0 36.2 9.4 46.6 24.9zM80 128V432c0 17.7 14.3 32 32 32H336c17.7 0 32-14.3 32-32V128H80zm80 64V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16zm80 0V400c0 8.8-7.2 16-16 16s-16-7.2-16-16V192c0-8.8 7.2-16 16-16s16 7.2 16 16z"/>
+          </svg>
+          </button>
+
         </div>
       </div>
     </div>
@@ -106,8 +111,7 @@ export default {
 
 .content {
   margin-left: 200px; /* Platz für die Navigationsleiste einräumen */
-  padding: 10px;
-  width: 80%;
+  width: 100%;
 }
 
 
@@ -167,15 +171,15 @@ export default {
   align-items: center;
   border: 5px solid #20a8a8;
   padding: 2px;
-margin-block: 10px;
-margin-right: 50px;
+  margin-block: 10px;
+  margin-right: 90px;
 }
 .file-list-header{
 align-items: center;
+width: 95%;
 justify-content: space-between;
 display:flex;
 margin-block: 10px;
-margin-right: 100px;
 margin-left:60px;
 }
 .file-name {
@@ -220,12 +224,12 @@ margin-left:60px;
 
 
 .delete-button {
-  background-color: rgb(63, 158, 170);
-  border-radius: 30px;
+border-radius: 50px;
   color: white;
   border: none;
+  margin-right: 20px;
   cursor: pointer;
-  width: 250px;
+  width: 50px;
   height: 50px;
 }
 </style>
