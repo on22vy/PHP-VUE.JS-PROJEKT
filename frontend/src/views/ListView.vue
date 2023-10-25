684d33d365a01e4ff5c72f65da32f500ee7fedc5
Acelya <acelya.cagan@gmail.com>
2023-10-25 16:13:14 +0200

    Add delete and download Button

--- frontend/src/views/ListView.vue
+++ frontend/src/views/ListView.vue
@@ -8,10 +8,29 @@
 <template>
     
     <h2>List View </h2>
+    <div class="delete">
+        <span class="material-icons">delete</span>
+ 
+    </div>
+    <div class="download">
+        <span class="material-icons">download</span>
+    </div>
 
-   
-   </template>
-   
+
+</template>
+
 <style scoped>
+
+.delete{
+ position: absolute;
+ height:320px;
+ right:150px;
+}
+.download{
+ position: absolute;
+ height:320px;
+ right:100px;
+}
+
 
 </style>
