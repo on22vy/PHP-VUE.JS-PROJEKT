f0acc8a34b816d8dfc8f4ad3bf431fc869cd2e9a
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-10-25 10:08:19 +0200

    Add file upload functionality

---
+++ backend/uploadFile.php
@@ -0,0 +1,13 @@
+<?php
+  
+    header('Access-Control-Allow-Origin: *');  
+  
+    if (move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/".$_FILES['file']['name'])) {
+        echo "done";
+        exit;
+    }
+  
+    echo "failed";  
+    
+  
+?>
