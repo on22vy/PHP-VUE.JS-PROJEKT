e53876282d19e568cd6334b6b9bf7478a2ffd9f5
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-02 12:57:27 +0100

    Add metadata of uploaded files into database

--- backend/uploadFile.php
+++ backend/uploadFile.php
@@ -1,13 +1,27 @@
 <?php
-  
+
     header('Access-Control-Allow-Origin: *');  
-  
+    require_once('dbconnect.php');
+    
     if (move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/".$_FILES['file']['name'])) {
-        echo "done";
-        exit;
-    }
-  
-    echo "failed";  
+        $filename = $_FILES['file']['name'];
+        $filesize = $_FILES['file']['size'];
+        $path_to_file = "uploads/" . $filename; // Set the file path
+        $created_date = date('Y-m-d H:i:s');
+        $sql = "INSERT INTO files (filename, filesize, created_date, path_to_file) VALUES (?, ?, ?, ?)";
+        
+        $test = $mysql->prepare($sql);
+        $test->bind_param("siss", $filename, $filesize, $created_date, $path_to_file);
+
+        if ($test->execute()) {
+            echo "Data inserted successfully!";
+        } else {
+            echo "Failed to insert data: " . $test->error;
+        }
+
+    }else{
+        echo "Failed to move data"; 
+    } 
     
   
 ?>
