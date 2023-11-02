613f8f72a6acad90a7102dcbd15a9fb7d4a88892
Acelya <acelya.cagan@gmail.com>
2023-11-02 21:15:17 +0100

    Add logic for openFile.php

--- backend/openFile.php
+++ backend/openFile.php
@@ -1,24 +1,16 @@
 <?php
-// openfile.php
+$uploadDirectory = "uploads/"; // Pfad zum Upload-Ordner auf Ihrem Server
+$fileList = [];
 
-// Datei aus der Datenbank abrufen
-if (isset($_GET['file_id'])) {
-  $fileId = $_GET['file_id']; // Eindeutige Kennung für die Datei
-
-  // Hier sollten Sie eine Datenbankabfrage durchführen, um den Dateipfad basierend auf $fileId zu erhalten
-  // Führen Sie die Abfrage aus und erhalten Sie den Dateipfad
-
-  if ($result) {
-    $file = $result->fetch_assoc();
-    if ($file) {
-      $filePath = $file['file_path'];
-      
-      // Öffnen der Datei im Browser
-      header('Content-type: application/pdf'); // Anpassen des Content-Type je nach Dateityp
-      header('Content-Disposition: inline; filename="' . basename($filePath) . '"');
-      readfile($filePath);
-      exit;
+if ($handle = opendir($uploadDirectory)) {
+    while (false !== ($file = readdir($handle))) {
+        if ($file != "." && $file != "..") {
+            $fileList[] = $file;
+        }
     }
-  }
+    closedir($handle);
 }
+
+header('Content-Type: application/json');
+echo json_encode($fileList);
 ?>
