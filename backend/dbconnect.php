c9fdd11a27bdfd10cb28ee7e1581f1574f4be3ce
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-02 12:46:36 +0100

    Add php code to connect with created database

---
+++ backend/dbconnect.php
@@ -0,0 +1,22 @@
+<?php
+//These are the defined authentication environment in the db service
+
+// The MySQL service named in the docker-compose.yml.
+$host = 'db';
+
+// Database use name
+$user = 'MYSQL_USER';
+
+//database user password
+$pass = 'MYSQL_PASSWORD';
+
+$database = 'MYSQL_DATABASE';
+
+// check the MySQL connection status
+$mysql= new mysqli($host, $user, $pass, $database);
+if ($mysql->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected to MySQL server successfully!";
+}
+?>
