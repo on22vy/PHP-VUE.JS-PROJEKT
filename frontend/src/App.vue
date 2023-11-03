2d783afa9f84db016cfc4ea2f336e750ff08f61c
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-03 13:41:18 +0100

    Removed unused comments in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,3 @@
-<!-- <template>
-  <ListStructure />
- 
-</template>
-
-<script>
-import ListStructure from './components/ListStructure.vue';
-
-export default {
-  name: 'App',
-  components: {
-
-    ListStructure
-}
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  color: #377bab;
-  margin-top: 0;
-}
-</style> -->
-
-
 <template>
 	<div class="app">
 		<!-- Sidebar -->
@@ -32,6 +6,8 @@ export default {
 		<!-- Content -->
 		<router-view />
 	</div>
+
+	
 </template>
 
 <script setup>
