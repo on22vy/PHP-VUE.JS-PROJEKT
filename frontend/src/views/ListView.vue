9c7d800f852e9887ccb3cecec323f5e439ee9640
Acelya <acelya.cagan@gmail.com>
2023-11-04 11:46:19 +0100

    Add delete and download Button, change style for the table

--- frontend/src/views/ListView.vue
+++ frontend/src/views/ListView.vue
@@ -1,138 +1,156 @@
-
 <template>
-  <div>
-    <h2>Liste der Dateien</h2>
-    <div class="file-list"></div>
-    <ul>
-      <li v-for="(file, index) in fileList" :key="index" class="file-item">
-        <a :href="file.url" target="_blank" class="file-link">{{ file.name }} </a>
-       
-        <div class="button-group">
-         <span class="downloadButton material-icons" @click="downloadFile(file)">download</span>
-         <span class="deleteButton material-icons" @click="() => deleteFile(file)">delete</span>
-        </div>
-      </li>
-    </ul>
-  </div>
+  <!-- <p>Test Grid</p> -->
+  <div class="fileView">
+          
+           <table>
+              <thead>
+              <tr>
+                  <th class="left">Filename</th>
+                  <th class="center">Filesize</th>
+                  <th class="right">Created</th>
+                  <th class="actionHeader"></th>
+                </tr>
+              </thead>
+              <tbody>
+                  <tr v-for="file in files" :key="file.id">
+                    <td class="left">
+                    <a :href="'../'+'php/'+file.path_to_file" target="_blank" class="file-name">
+                      {{ file.filename }}
+                      </a>
+                      </td>
+                      <td class="center">{{ file.filesize }}</td>
+                      <td class="right">{{ file.created_date  }}</td>
+                      <td class="right">
+                     <button class="downloadButton material-icons">download</button>
+                      <button class="deleteButton material-icons">delete</button>
+          </td>
+                   
+                  </tr>
+
+              </tbody>
+          </table> 
+
+   
+    </div>
 </template>
 
-<script>
-import axios from 'axios';
+<script setup>
+  import axios from 'axios';
+  import { ref, onMounted } from 'vue';
 
-export default {
-  data() {
-    return {
-      fileList: [],
-    };
-  },
+  const files = ref("");
 
-  methods: {
-      async fetchFileList() {
-  try {
-    const response = await axios.get('/php/openFile.php'); // Relativer Pfad zur PHP-Datei
-    console.log(response.data); // Überprüfen Sie die Serverantwort
-    const files = response.data;
-    this.fileList = files.map(file => ({
-      name: file,
-      url: `/uploads/${file}`, // Stellen Sie sicher, dass der Pfad zum Upload-Ordner korrekt ist
-    }));
-  } catch (error) {
-    console.error('Fehler beim Abrufen der Dateiliste:', error);
+  
+
+  const getAllFiles = () => {
+    axios.get('http://localhost:8000/php/getFilesList.php', {
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then((response) => {
+        files.value = response.data
+        setTimeout(getAllFiles, 1000);
+    })
+    .catch(function(error){
+        console.error("Failed to fetch data", error)
+    });
   }
-},
-downloadFile(file) {
-  const a = document.createElement('a');
   
-  // Setzen Sie die URL der ausgewählten Datei (file, nicht this.files)
-  a.href = file.url;
-
-  // Geben Sie der Datei einen Namen für den Download (file.name, nicht selectedFile.name)
-  a.download = file.name;
-
-  // Fügen Sie das Link-Element zum Dokument hinzu
-  document.body.appendChild(a);
-
-  // Klicken Sie auf den Link, um den Download zu starten
-  a.click();
-
-  // Entfernen Sie das Link-Element aus dem Dokument
-  document.body.removeChild(a);
-}
-},
-deleteFile(file) {
-      
-      axios.post('/php/deleteFile.php', { filename: file.name })
-        .then(response => {
-          if (response.data.success) {
-            // Erfolgreich gelöscht, aktualisieren Sie die Dateiliste
-            this.fetchFileList();
-          } else {
-            // Fehler beim Löschen
-            console.error('Fehler beim Löschen der Datei.');
-          }
-        })
-        .catch(error => {
-          console.error('Fehler beim Löschen der Datei:', error);
-        });
-    },
   
-created() {
-    this.fetchFileList();
-  },
-};
+  onMounted(()=>
+  {
+    getAllFiles();
+  });
+
+ 
+
+  // const showFile = (path) => {
+  //   // Implement a mechanism to display the file when clicked.
+  //   // You can open it in a new tab, use a modal, or other approaches.
+  //   window.open(path, '_blank');
+  // };
+
 </script>
 
 <style scoped>
 
-.deleteButton,
 .downloadButton {
   cursor: pointer;
   padding: 5px; /* Füge etwas Abstand zu den Icons hinzu */
   margin-left: 10px; /* Füge Abstand zwischen den Buttons und den anderen Elementen hinzu */
 }
-
-.file-link{
-  color: black;
-  flex:1;
-}
-.file-item {
-  display: flex;
-  width: 100%;
-  justify-content: space-between;
-  align-items: center;
-  padding: 2px;
-  border: 2px solid var(--light);
-  background-color: var(--light);
-  margin-block: 10px;
-
-}
-.button-group {
-  display: flex;
-  align-items: center;
-  gap: 10px; /* Abstand zwischen den Buttons anpassen */
-}
-.last-changed-date {
-  flex: 2;
-  text-align: center;
-}
-
-.file-name {
-  flex: 1;
-  text-decoration: none;
+.deleteButton {
   cursor: pointer;
-  color: var(--dark);
+  padding: 5px; /* Füge etwas Abstand zu den Icons hinzu */
+  margin-left: 10px; /* Füge Abstand zwischen den Buttons und den anderen Elementen hinzu */
+
 }
+
 
 .file-name:hover {
   text-decoration: underline;
 }
 
-.user-name {
-  flex: 3;
-}
 
-.file-list {
-  padding: 10px;
-}
+  .fileView {
+    background-color: #f2f2f2; /* Hintergrundfarbe für die Dateiansicht */
+    padding: 10px; /* Innenabstand */
+  }
 
+  table {
+    width: 100%; /* Volle Breite des Elternelements einnehmen */
+    border-collapse: collapse; /* Tabellenzellen zusammenfassen */
+  }
+
+  th {
+    text-align: left; /* Text links ausrichten */
+    padding: 8px; /* Innenabstand für Zellen */
+    background-color: #ccc; /* Hintergrundfarbe für Überschriften */
+
+  }
+  td {
+    text-align: center;
+    padding:8px;
+  }
+  th.left {
+    text-align: left;
+  }
+
+  th.center {
+    text-align: center;
+  }
+
+  th.right {
+    text-align: right;
+  }
+
+th.actionHeader {
+  text-align: right;
+  }
+  tr:nth-child(even) {
+    background-color: #fff; /* Hintergrundfarbe für gerade Zeilen */
+  }
+
+  tr:nth-child(odd) {
+    background-color: #f2f2f2; /* Hintergrundfarbe für ungerade Zeilen */
+  }
+  td.left {
+    text-align: left;
+  }
+
+  td.center {
+    text-align: center;
+  }
+
+  td.right {
+    text-align: right;
+  }
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+
+ 
 </style>
