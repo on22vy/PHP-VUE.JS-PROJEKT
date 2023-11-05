ea67bc5d51bc3d5a3e8271299e99a25f8c42ab94
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-05 01:26:13 +0100

    dbconnect.php: Removed the success message

--- backend/dbconnect.php
+++ backend/dbconnect.php
@@ -18,7 +18,4 @@ $mysql= new mysqli($host, $user, $pass, $database);
 if ($mysql->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-// } else {
-//     echo "Connected to MySQL server successfully!";
-// }
 ?>
