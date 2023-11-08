62731b73d3025dc7300f29c7c2ddc46fa6ace968
Acelya <acelya.cagan@gmail.com>
2023-11-08 19:49:34 +0100

    Add author name to deleteFile.php

--- backend/deleteFile.php
+++ backend/deleteFile.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ * @author Acelya Cagan <acelya.cagan@gmail.com>
+*/
 // Einstellungen für CORS
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
