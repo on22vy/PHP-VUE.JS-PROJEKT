7e70e21693eee819a9fd58d324d718ebd7347b4a
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-09 17:14:01 +0100

    Created Login Page

---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,25 @@
+<script setup>
+/**
+ * @author Thi Tuong Vy Nguyen <thi.nguyen.22@lehre.mosbach.dhbw.de>
+ */
+// import { useRouter } from 'vue-router';
+
+// const router = useRouter();
+
+// const login = () => {
+//   // Logic for handling login
+
+//   // Assuming login is successful, navigate to the app page
+//   router.push('/app');
+// };
+</script>
+
+<template>
+	<h1>Login Page</h1>
+    <!-- <Button @click="login()">Login</Button> -->
+    <router-link to="/home">Login</router-link>
+
+	
+</template>
+<style scoped>
+</style>
