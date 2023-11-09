94eddb193a15dd03d1263558372b8fc5040839ca
Acelya <acelya.cagan@gmail.com>
2023-11-09 18:04:51 +0100

    Change path for import

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,8 @@
  * @author Thi Tuong Vy Nguyen <thi.nguyen.22@lehre.mosbach.dhbw.de>
  */
 import { createApp } from 'vue'
-import LogIn from './views/LogIn.vue'
+import App from './App.vue'
 import router from './router/router.js'
 
 
-createApp(LogIn).use(router).mount('#app')
+createApp(App).use(router).mount('#app')
