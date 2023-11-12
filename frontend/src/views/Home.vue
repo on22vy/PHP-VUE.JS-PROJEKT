1c36a163ec135ab8977828dbb91c0251c2283057
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-12 02:33:41 +0100

    Adjusted Styling of Home Page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,24 +3,47 @@
  * @author Thi Tuong Vy Nguyen <thi.nguyen.22@lehre.mosbach.dhbw.de>
  */
 import Toolbar from '../components/Toolbar.vue'
+import Sidebar from '../components/Sidebar.vue'
 </script>
 
 <template>
     <main class="homePage">
-       <h1>Home</h1>
-       <p>This is the home page</p>
-       <div class="tools">
+       
+       <!-- Sidebar -->
+		  <Sidebar />
+      <div class="content">
+        <div class="tools">
          <!-- Toolbar -->
          <Toolbar />
        </div>
        <div class="filesContainer">
          <!-- Entweder FileStructur oder Kachel -->
        </div>
+      </div>
+       
    
     </main>
 </template>
 
 <style scoped>
+.homePage {
+  display: flex;
+  :is(main) {
+		flex: 1 1 0;
+		padding: 2rem;
+
+		@media (max-width: 1024px) {
+			padding-left: 6rem;
+		}
+	}
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  flex: 1; /* Take remaining space */
+  margin: 2rem;
+}
 
 </style>
    
