c24e8fb44dde7483f9105c8f4383a8349a6d0625
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-23 22:29:55 +0100

    remove unused comment

--- backend/renameFile.php
+++ backend/renameFile.php
@@ -3,7 +3,7 @@
  * @author Acelya Cagan <acelya.cagan@gmail.com>
  */
 
-// Einstellungen für CORS
+
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
