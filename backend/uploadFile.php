b893e41462a77370c99245f1549496641c6bf532
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-23 22:30:46 +0100

    Add Comments to uploadFile.php

--- backend/uploadFile.php
+++ backend/uploadFile.php
@@ -3,18 +3,26 @@
  * @author Thi Tuong Vy Nguyen <thi.nguyen.22@lehre.mosbach.dhbw.de>
  */
     header('Access-Control-Allow-Origin: *');  
+
+    // Including the database connection file
     require_once('dbconnect.php');
     
+    // Checking if the file was successfully uploaded: Check if the file was successfully moved from the temporary directory to the desired destination
     if (move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/".$_FILES['file']['name'])) {
+        // Extracting information about the uploaded file
         $filename = $_FILES['file']['name'];
         $filesize = $_FILES['file']['size'];
         $path_to_file = "uploads/" . $filename; // Set the file path
         $created_date = date('Y-m-d H:i:s');
+
+        // SQL query to insert file information into the database
         $sql = "INSERT INTO files (filename, filesize, created_date, path_to_file) VALUES (?, ?, ?, ?)";
         
+        // Preparing and binding parameters for the SQL query
         $test = $mysql->prepare($sql);
         $test->bind_param("siss", $filename, $filesize, $created_date, $path_to_file);
 
+        // Executing the prepared SQL query
         if ($test->execute()) {
             echo "Data inserted successfully!";
         } else {
@@ -22,7 +30,7 @@
         }
 
     }else{
-        echo "Failed to move data"; 
+        echo "Failed to move data";  // Output message if file upload fails
     } 
     
   
