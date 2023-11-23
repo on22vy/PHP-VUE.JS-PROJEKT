506310b6871542d241d4901208a44a88fe2d73ef
on22vy <thi.nguyen.22@lehre.mosbach.dhbw.de>
2023-11-23 19:52:56 +0100

    Add comments to logout.php

--- backend/logout.php
+++ backend/logout.php
@@ -2,8 +2,13 @@
 /**
  * @author Thi Tuong Vy Nguyen <thi.nguyen.22@lehre.mosbach.dhbw.de>
  */
+// Start the session to access session variables
 session_start();
+
+// Unset all session variables
 session_unset();
+
+// Destroy the session
 session_destroy();
 
 die();
